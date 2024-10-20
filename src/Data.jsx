0e624a81ca6f26e9df01d26b2cc3f011e9838a4d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-10-20 13:48:12 +0530

    Updated website icon and added all the images and data file

---
+++ src/Data.jsx
@@ -0,0 +1,173 @@
+import Bike1 from './assets/1.png';
+import Bike2 from './assets/2.png';
+import Bike3 from './assets/3.png';
+import Bike4 from './assets/4.png';
+import Bike5 from './assets/5.png';
+import Bike6 from './assets/6.png';
+
+import Testimonial1 from './assets/testimonial1.jpg';
+import Testimonial2 from './assets/testimonial2.jpg';
+import Testimonial3 from './assets/testimonial3.jpg';
+import Testimonial4 from './assets/testimonial4.jpg';
+import Testimonial5 from './assets/testimonial5.jpg';
+
+export const links = [
+  {
+    name: 'Home',
+    path: 'home',
+  },
+  {
+    name: 'About Us',
+    path: 'about',
+  },
+  {
+    name: 'Products',
+    path: 'products',
+  },
+  {
+    name: 'Testimonials',
+    path: 'testimonials',
+  },
+  {
+    name: 'Call-to-Action',
+    path: 'call',
+  },
+  {
+    name: 'Contact Us',
+    path: 'contact',
+  }
+];
+
+export const services = [
+  {
+    id: 1,
+    name: 'Mountain Bikes',
+    title: 'Off-Road Adventure',
+    description:
+      'Engineered for durability and stability, our mountain bikes offer excellent performance on rugged trails. Built with advanced suspension systems and lightweight frames for the ultimate off-road experience.',
+  },
+  {
+    id: 2,
+    name: ' Electric Bikes ',
+    title: ' Eco-Friendly Commuting ',
+    description:
+      'Electric bikes with pedal-assist technology provide an efficient and eco-friendly way to commute. Equipped with long-lasting batteries and smooth motor systems for effortless travel.',
+  },
+  {
+    id: 3,
+    name: ' Hybrid Bikes ',
+    title: ' Versatile & Comfortable ',
+    description:
+      ' A blend of road and mountain bikes, hybrid bikes are ideal for city streets and light trails. Comfortable seating and adaptable features make them perfect for casual riders and daily commuters.',
+  },
+  {
+    id: 4,
+    name: ' Kids Bikes',
+    title: ' Safe & Simple',
+    description:
+      ' Our kids bikes are designed with safety in mind, offering sturdy frames, easy-to-use brakes, and colorful designs that children will love. Perfect for learning and outdoor fun ',
+  },
+  {
+    id: 5,
+    name: ' Road Bikes ',
+    title: ' Speed & Performance ',
+    description:
+      'Designed for speed and long-distance rides, our road bikes feature lightweight materials and aerodynamic designs. Perfect for competitive racing or high-speed commuting.',
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    img: Bike1,
+    category: 'Road Bike',
+    title: 'Speedster',
+    description:
+      'The Speedster is lighter and more stylish than ever before. The frame kit features a carbon fork, internal cable.',
+  },
+  {
+    id: 2,
+    img: Bike2,
+    category: 'Mountain Bike',
+    title: 'Trail Blazer',
+    description:
+      'You are ready for anything. Tough climbs, rowdy descents and big backcountry rides. Trance thrives on all types of trails.',
+  },
+  {
+    id: 3,
+    img: Bike3,
+    category: 'Hybrid Bike',
+    title: 'City Cruiser',
+    description:
+      'Navigate the cityscape with ease, thanks to the ergonomic design and efficient functionality of our City Cruiser Bikes.',
+  },
+  {
+    id: 4,
+    img: Bike4,
+    category: 'Electric Bike',
+    title: 'Eco Rider',
+    description:
+      'The Explorer GT takes quad power to a new level, with 4-wheel direct drive and a 3000w brushless motor for reliably generating huge power and torque.',
+  },
+  {
+    id: 5,
+    img: Bike5,
+    category: 'Kids Bike',
+    title: 'Little Explorer',
+    description:
+      'Make your ride out a real family affair by using the Oxford top-tube mounted Little Explorer. · Max tube width - 50mm · Suitable for ages 3 to 6 years ',
+  },
+  {
+    id: 6,
+    img: Project6,
+    category: 'Folding Bike',
+    title: 'Compact Commuter',
+    description:
+      'These convenient and compact bikes make travel, commuting, and life much easier.List includes: The Best Folding Bikes',
+  },
+];
+
+
+
+export const testimonials = [
+  {
+    id: 1,
+    img: Testimonial1,
+    name: 'Barbara Wilson',
+    author: 'CEO Company',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 2,
+    img: Testimonial2,
+    name: 'Charlie Smith',
+    author: 'Designer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 3,
+    img: Testimonial3,
+    name: 'Roy Wang',
+    author: 'Manager GYM',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 4,
+    img: Testimonial4,
+    name: 'Jennifer Smith',
+    author: 'CEO & Founder',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 5,
+    img: Testimonial5,
+    name: 'Paul Freeman',
+    author: 'Photographer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
